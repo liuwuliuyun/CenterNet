08b41cbf511cb568fde314bc93c06f14b412335c
Yun Liu <liuwuliuyun@gmail.com>
2019-09-18 12:46:17 +0800

    Update ctdet_coco_msdcn.sh

--- experiments/ctdet_coco_msdcn.sh
+++ experiments/ctdet_coco_msdcn.sh
@@ -1,3 +1,5 @@
 cd ../src
 # train
-python main.py ctdet --exp_id coco_msdcn --arch mobiledcn --batch_size 32 --lr 5e-4 --gpus 0 --num_workers 4 --save_all
+python3 main.py ctdet --exp_id coco_msdcn_2 --arch mobiledcn --batch_size 32 --lr 1e-4 --gpus 0 --num_workers 4 --save_all --lr_step 40,80 --input_res 512
+# test
+python3 test.py ctdet --exp_id coco_msdcn --arch mobiledcn --resume --flip_test --test_scales 0.5,0.75,1,1.25,1.5
